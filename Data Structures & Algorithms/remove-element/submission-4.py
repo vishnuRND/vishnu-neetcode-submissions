4b208baacb8ae3d44ee3ceeabf9db42cec43bef3
vishnuRND <vishnubvrit@gmail.com>
2026-09-18 16:39:44 +0530

    Add: remove-element - submission-4

---
+++ Data Structures & Algorithms/remove-element/submission-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        lastUniqueIndex = -1
+        for j in range(len(nums)):
+            if nums[j] != val:
+                nums[lastUniqueIndex+1] = nums[j]
+                lastUniqueIndex += 1
+            else:
+                continue
+        return lastUniqueIndex+1
+
